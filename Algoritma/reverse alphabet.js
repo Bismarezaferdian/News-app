88a3f2106c660f4a1c030e24da4e39a9de18f9a3
Bisma Reza Ferdian <48860471+Bismarezaferdian@users.noreply.github.com>
2023-04-02 02:01:36 +0700

    Update and rename reverse alphabet to reverse alphabet.js

---
+++ Algoritma/reverse alphabet.js
@@ -0,0 +1,12 @@
+
+const str = "NEGIE1";
+let alpha = "";
+for (let i = 0; i < str.length; i++) {
+//cek jika str[i] huruf push ke variable alpha
+if (isNaN(parseInt(str[i]))) {
+alpha += str[i];
+}
+}
+//convert string ke array lalu reverse
+const reverse= alpha.split("").reverse()//["E", "I", "G", "E", "N"]
+console.log(reverse.join("")); //"EIGEN"
