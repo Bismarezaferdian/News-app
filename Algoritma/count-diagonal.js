06cee3129653625212f420c485a9cee7a183b6af
Bisma Reza Ferdian <48860471+Bismarezaferdian@users.noreply.github.com>
2023-04-02 21:19:49 +0700

    Update count-diagonal.js

--- Algoritma/count-diagonal.js
+++ Algoritma/count-diagonal.js
@@ -1,4 +1,4 @@
-//Silahkan cari hasil dari pengurangan dari jumlah diagonal sebuah matrik NxN Contoh:
+//4Silahkan cari hasil dari pengurangan dari jumlah diagonal sebuah matrik NxN Contoh:
 //Matrix = [[1, 2, 0], [4, 5, 6], [7, 8, 9]]
 
 //diagonal pertama = 1 + 5 + 9 = 15 
